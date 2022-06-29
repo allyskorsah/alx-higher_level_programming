5f6bab25a84648fa89f8d531349cf95314899854
allyskorsah <allyskorsah>
2022-06-29 04:20:30 -0700

    corrrections

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
 else:
     last = number % 10
 if last > 5:
-    print(f'Last digit of {number} is {last} and is greater than 5 \n')
+    print(f'Last digit of {number} is {last} and is greater than 5')
 elif last == 0:
-    print(f'Last digit of {number} is {last} and is 0\n')
+    print(f'Last digit of {number} is {last} and is 0')
 elif last < 6 and last != 0:
-    print(f'Last digit of {number} is {last} and is less than 6 and not 0\n')
+    print(f'Last digit of {number} is {last} and is less than 6 and not 0')
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
     if numbers != 99:
-        print(f'{numbers:02}', end=", ")
+        print('{:02d}'.format(numbers), end=", ")
     else:
-        print(f'{numbers}')
+        print('{:d}'.format(numbers))
